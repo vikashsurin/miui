eab8a5658cf67e7625c343c56d368bf62ec13885
VikashSurin <vikashsurin10@gmail.com>
2025-07-30 14:52:36 +0530

    updated

---
+++ dist/styles/size.d.ts
@@ -0,0 +1,14 @@
+export declare const sizes: {
+    xs: string;
+    sm: string;
+    base: string;
+    lg: string;
+    xl: string;
+    '2xl': string;
+    '3xl': string;
+    '4xl': string;
+    '5xl': string;
+    '6xl': string;
+    '7xl': string;
+    '8xl': string;
+};
---
+++ dist/ui/button/Button.svelte.d.ts
@@ -0,0 +1,7 @@
+declare const Button: import("svelte").Component<{
+    uiSize: any;
+    children: any;
+    class: any;
+} & Record<string, any>, {}, "">;
+type Button = ReturnType<typeof Button>;
+export default Button;
